28546ec1c2f1690e978f44f053fb7d606f071539
halfalpine <rocksandwoks@gmail.com>
2016-09-05 14:22:40 -0400

    Add Wikipedia links to page

--- js/script.js
+++ js/script.js
@@ -15,11 +15,13 @@ function loadData() {
       return cityStr.slice(0, commaIndex);
     };
 
-    var nytURL = "https://api.nytimestttttttt.com/svc/search/v2/articlesearch.json";
+    var nytURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     nytURL += '?' + $.param({
       'api-key': "53583737f41c443da8cce64008269fad",
       'q': pureCity(city)
     });
+
+    var wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&meta=&indexpageids=1&generator=search&gsrsearch=' + city + '&gsrlimit=10&gsrprop=size%7Cwordcount%7Ctimestamp%7Csnippet%7Ctitlesnippet&callback=?';
 
     // clear out old data before new request
     $wikiElem.text("");
@@ -34,7 +36,7 @@ function loadData() {
       $.each(data.response.docs, function(key, value) {
         var headline = value.headline.main;
         var lead = value.lead_paragraph || 'Click link for content.';
-        items.push('<li class="article"><a class="title" href="#">' + headline + '</a><p class="lead">' + lead + '</p></li>');
+        items.push('<li class="nyt-article"><a class="title" href="#">' + headline + '</a><p class="lead">' + lead + '</p></li>');
       });
       $('#nytimes-articles').append(function() {
         return items.join('');
@@ -42,6 +44,23 @@ function loadData() {
     }).fail(function() {
       console.log('foo');
       $('#nytimes-articles').html('Articles coule not be loaded!');
+    });
+
+    $.ajax({
+      url: wikiURL,
+      dataType: 'json',
+      success: function(data) {
+        var pages = data.query.pages;
+        var items = [];
+        for (var id in pages) {
+          console.log(id);
+          items.push('<li class="wiki-article"><a class="wiki-link" href="http://en.wikipedia.org/?curid=' + id + '" target="_blank">' + pages[id].title + '</a></li>');
+        }
+        console.log(items);
+        $('#wikipedia-links').append(function() {
+          return items.join('');
+        });
+      }
     });
 
     return false;
